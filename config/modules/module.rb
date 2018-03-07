9a818bb5b8a517725d0c119b64259d3976423fe6
tao-k <tao@sitebridge.co.jp>
2018-03-07 16:01:17 +0900

    fix module name

--- config/modules/module.rb
+++ config/modules/module.rb
@@ -1,10 +1,10 @@
-Cms::Lib::Modules::ModuleSet.draw :login, 'ログイン情報：プラグイン', 999 do |mod|
+Cms::Lib::Modules::ModuleSet.draw :login, 'ログイン情報', 999 do |mod|
 #  ## contents
-  mod.content :users, 'ログイン情報：プラグイン'
+  mod.content :users, 'ログイン情報'
 
 #  ## directories
-  mod.directory :users, 'ログイン：プラグイン'
+  mod.directory :users, 'ログイン'
 
   ## pieces
-  mod.piece :users, 'ログイン：プラグイン'
+  mod.piece :users, 'ログイン'
 end
